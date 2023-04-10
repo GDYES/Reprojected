db970439d33a7670d5f92cdd2f206ca3ef1a3c34
nuri (smol) <75618732+SMOLKEYS@users.noreply.github.com>
2023-04-10 17:12:00 +0800

    Update and rename mod-src/src/main/kotlin/ExampleMod.kt to mod-src/src/main/kotlin/repj/Reprojected.kt

---
+++ mod-src/src/main/kotlin/repj/Reprojected.kt
@@ -0,0 +1,16 @@
+package repj
+
+import arc.*
+import arc.scene.ui.*
+import mindustry.mod.*
+import mindustry.game.*
+import mindustry.ui.*
+import mindustry.ui.dialogs.*
+
+class Reprojected : Mod() {
+
+	init {
+		//time is needed
+	}
+	
+}
