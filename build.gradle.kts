b2cbf54dc2443f4b831edd5f06e0a5d5dcd76095
nuri (smol) <75618732+SMOLKEYS@users.noreply.github.com>
2023-04-10 17:06:14 +0800

    prepping 2: cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,38 +7,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  */
 
 plugins {
-	// when modifying the version of this plugin, you must also modify the version in other buildscripts:
-	// all modules must share the same version or the compilation will fail
 	kotlin("jvm") version "1.8.0"
 }
 
 allprojects {
-	// this one applies to every script, its version is inherited from the kotlin plugin version.
 	apply(plugin = "org.jetbrains.kotlin.jvm")
 
 	repositories {
-		// Repositories to download dependencies from.
-		// Do not remove anything other than mavenLocal unless you knoa what you're doing
 		mavenCentral()
 		mavenLocal()
 		maven("https://jitpack.io")
 	}
 
 	dependencies {
-		// be cautious: dependencies applied here are applied to every submodule
-		// never insert local project dependencies (e.g. implementation(project(":another-module"))).
-
-		// in addiction, never declare mindustry, arc and other mods (not libraries) as implementation deps!
-		// this will greatly increase the size of your mod and will most likely break something at runtime!
+		compileOnly("com.github.Anuken.Arc", "arc-core", "v143.1")
+		compileOnly("com.github.Anuken", "MindustryJitpack", "v143.1")
 		
-		// arc dependency
-		compileOnly("com.github.Anuken.Arc", "arc-core", "v141")
-		// mindustry dependency.
-		// jitpack refuses to compile mindustry due to its repository size, thus we're using a mirror
-		compileOnly("com.github.Anuken", "MindustryJitpack", "v141")
-		//example of a library dependency. if you don't need it, remove this line.
-		//(note: this is not a mod, it's a library for mindustry mods, thus it should be added as an implementation dependency.
-		implementation("com.github.mnemotechnician", "mkui", "v1.2.1")
+		//might use, we'll have to see.
+		//implementation("com.github.mnemotechnician", "mkui", "v1.2.1")
 	}
 
 	tasks.withType<JavaCompile> {
